5b2facdac73aed3d91758eda0cbb149ed2ee4f91
minajor-boi <70046323+minajor-boi@users.noreply.github.com>
2021-10-24 09:46:04 +0300

    Add files via upload

---
+++ Console Programlama/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace c
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.Write("İsim girin: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("Merhaba: " + name);
+        }
+    }
+}
